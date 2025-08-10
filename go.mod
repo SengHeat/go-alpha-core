ae4d5f5c2ac0ed56011a5f2c7ef7edbb9eec6ff3
SengHeat <heatblack009@example.com>
2025-08-10 10:33:23 +0700

    chore: add configuration database

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module alpha-core
 
 go 1.23
+
+require github.com/joho/godotenv v1.5.1
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,65 @@
 package config
+
+import (
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	AppEnv  string
+	AppPort string
+
+	DBHost string
+	DBPort string
+	DBUser string
+	DBPass string
+	DBName string
+
+	JWTSecret        string
+	JWTExpiryMinutes int
+}
+
+func Load() *Config {
+	_ = godotenv.Load()
+
+	config := &Config{
+		AppEnv:           getEnv("APP_ENV", "development"),
+		AppPort:          getEnv("APP_PORT", "8080"),
+		DBHost:           getEnv("DB_HOST", "localhost"),
+		DBPort:           getEnv("DB_PORT", "5432"),
+		DBUser:           getEnv("DB_USER", "postgres"),
+		DBPass:           getEnv("DB_PASS", "password"),
+		DBName:           getEnv("DB_NAME", "myapp"),
+		JWTSecret:        getEnv("JWT_SECRET", "mysecret"),
+		JWTExpiryMinutes: getEnvAsInt("JWT_EXPIRY_MINUTES", 60),
+	}
+
+	return config
+}
+
+func getEnv(key, defaultVal string) string {
+	env := os.Getenv(key)
+
+	if env == "" {
+		return defaultVal
+	}
+
+	return env
+}
+
+func getEnvAsInt(key string, defaultVal int) int {
+	env := os.Getenv(key)
+
+	if env == "" {
+		return defaultVal
+	}
+
+	value, err := strconv.Atoi(env)
+	if err != nil {
+		return defaultVal
+	}
+
+	return value
+}
