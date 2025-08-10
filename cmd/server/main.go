c8280601f1b002b041717b1586abe20043cebc56
SengHeat <heatblack009@example.com>
2025-08-10 11:45:24 +0700

    chore: custome logger level

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"alpha-core/internal/config"
+	"alpha-core/pkg/logger"
 	"fmt"
 )
 
 func main() {
 	configure := config.Load()
+	log := logger.Init(configure)
 
-	fmt.Print(configure)
+	fmt.Println(configure)
+
+	log.InfoLog("failed to connect db: %v", configure)
+	log.WarningLog("failed to connect db: %v", configure)
+	log.ErrorLog("failed to connect db: %v", configure)
+
 }
--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,1 +1,39 @@
 package logger
+
+import (
+	"alpha-core/internal/config"
+	"fmt"
+	"log"
+	"os"
+)
+
+const (
+	Reset  = "\033[0m"
+	Red    = "\033[31m"
+	Green  = "\033[32m"
+	Yellow = "\033[33m"
+)
+
+type Logger struct{}
+
+func Init(configure *config.Config) *Logger {
+	log.SetFlags(log.Ldate | log.Lmicroseconds)
+	_ = configure
+	return &Logger{}
+}
+
+func (logger *Logger) InfoLog(format string, args ...interface{}) {
+	msg := fmt.Sprintf(format, args...)
+	log.Println(string(Green) + "[INFO] : " + msg + string(Reset))
+}
+
+func (logger *Logger) WarningLog(format string, args ...interface{}) {
+	msg := fmt.Sprintf(format, args...)
+	log.Println(string(Yellow) + "[WARN] : " + msg + string(Reset))
+}
+
+func (logger *Logger) ErrorLog(format string, args ...interface{}) {
+	msg := fmt.Sprintf(format, args...)
+	log.Println(string(Red) + "[FAIL] : " + msg + string(Reset))
+	os.Exit(1)
+}
